c537e166857b16adead8ba233fb14fd718ccc12e
yoomster <nperenboom@hotmail.com>
2024-03-26 17:10:45 +0100

    reken oefeningen toegevoegd

--- Week1/Program.cs
+++ Week1/Program.cs
@@ -13,28 +13,32 @@ namespace Week1
         static void Main(string[] args)
         {
             //variabelen
-            string welcome = "Hi, welcome to this app";
-            Console.WriteLine(welcome);
+            //string welcome = "Hi, welcome to this app";
+            //Console.WriteLine(welcome);
 
-            Console.Write( "What is your age: ");
-            string ageNr = Console.ReadLine();
-            int.TryParse(ageNr, out int age);
-            Console.WriteLine($"your age in 15 years will be: {age + 15}" );
+            //Console.Write( "What is your age: ");
+            //string ageNr = Console.ReadLine();
+            //int.TryParse(ageNr, out int age);
+            //Console.WriteLine($"your age in 15 years will be: {age + 15}" );
 
-            //-------------------------------
-            Console.WriteLine(Add(10, 5));
-            Console.WriteLine(Add(79650, 450));
+            ////-------------------------------
+            //Console.WriteLine(Add(10, 5));
+            //Console.WriteLine(Add(79650, 450));
 
-            Console.WriteLine(Subtract(10, 5));
-            Console.WriteLine(Subtract(79650, 24400));
+            //Console.WriteLine(Subtract(10, 5));
+            //Console.WriteLine(Subtract(79650, 24400));
 
-            Console.WriteLine(Multiply(10, 5));
-            Console.WriteLine(Multiply(120, 120));
+            //Console.WriteLine(Multiply(10, 5));
+            //Console.WriteLine(Multiply(120, 120));
 
-            double multiplied = Divide(10, 5);
-            Console.WriteLine(multiplied);
-            Console.WriteLine(Divide(10, 3));
-            Console.WriteLine(Divide(79650, 11));
+            //double multiplied = Divide(10, 5);
+            //Console.WriteLine(multiplied);
+            //Console.WriteLine(Divide(10, 3));
+            //Console.WriteLine(Divide(79650, 11));
+
+
+            //RechtToeRechtAan();
+            //AlWandelend();
 
 
             Console.ReadLine();
@@ -56,5 +60,30 @@ namespace Week1
         {
             return a / b;
         }
+
+        public static void RechtToeRechtAan()
+        {
+            //double output = (100 * 0.7) - (25 * 0.6);
+
+            //double output = 18.0 / 5.0;
+
+            double output = 100 / 30;
+
+            Console.WriteLine(output);
+
+        }
+
+        public static void AlWandelend()
+        {
+            double afstand = 20;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                afstand = afstand - (afstand / 4);
+                Console.WriteLine($"resterende afstand na rustpunt {i} is {afstand}");
+            }
+
+        }
+
     }
 }
