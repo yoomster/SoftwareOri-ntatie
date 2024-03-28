fc5b13daa2f1dd47f39eef2abaa3cafbe1007e0f
yoomster <nperenboom@hotmail.com>
2024-03-28 17:51:15 +0100

    expanded calculator

--- Goeroe-calc/Program.cs
+++ Goeroe-calc/Program.cs
@@ -11,21 +11,55 @@ namespace Goeroe_calc
         static void Main(string[] args)
         {
             //vraag om getal 1
-            Console.WriteLine("Vul een getal in en druk op enter: ");
-            int.TryParse(Console.ReadLine(), out int number);
+            PrintMessage("Vul een getal in en druk op enter: ");
+            double.TryParse(Console.ReadLine(), out double number);
 
             //vraag om getal 2
             Console.WriteLine("Vul nog een getal in en druk opnieuw op enter: ");
-            int.TryParse(Console.ReadLine(), out int numberTwo);
+            double.TryParse(Console.ReadLine(), out double numberTwo);
 
             //vertel gebruiker hoeveel je krijgt als je de getallen optelt
-            Console.WriteLine("Door de opgegeven getallen op te tellen, krijg je " + (number + numberTwo));
+            Console.WriteLine("Uitkomst optellen: " + (number + numberTwo));
 
-            //maar ook hoeveel je krijgt als je de getallen vermenigvuldigt.
-            Console.WriteLine("Door dezelfde opgegeven getallen te vermenigvuldigen, krijg je " + (number * numberTwo));
+            //en als je de getallen vermenigvuldigt
+            Console.WriteLine("Uitkomst vermenigvuldigen: " + (number * numberTwo));
+
+            //door worteltrekken
+            SquareRoot(number);
+
+
+            Console.WriteLine("Uitkomst worteltrekken: " + (number * numberTwo));
+
+
+            //en door te delen
+            Console.WriteLine("Uitkomst delen: " + (number / numberTwo));
+
 
 
             Console.ReadLine();
+        }
+
+        public static void PrintMessage(string a)
+        {
+            Console.WriteLine(a);
+        }
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+       
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine(a * b);
+        }
+        
+
+        public static double SquareRoot(double a)
+        {
+            double output = Math.Sqrt(a);
+
+            return output;
         }
     }
 }
