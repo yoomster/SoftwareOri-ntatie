17baf399cf59048e16ecfb5dc4fd1c5f06d454fc
yoomster <nperenboom@hotmail.com>
2024-03-30 15:25:16 +0100

    Added new console project for week 2

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //String manipulatie (in C#)
+
+            Console.WriteLine("abcdef".Substring(1, 1));  
+            Console.WriteLine("abcdef".Substring(3, 2));
+            Console.WriteLine("abcdef".Substring(0, 6));
+            //Console.WriteLine("abcdef".Substring(0, 7));
+            //Console.WriteLine("abcdef".Substring(7, 1));
+
+
+            Console.ReadLine();
+        }
+    }
+}
