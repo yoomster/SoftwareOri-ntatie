809fc489dd43ca5a59d1d13ac8687fdacc5a1279
yoomster <nperenboom@hotmail.com>
2024-04-03 16:40:44 +0200

    Added form for finding the index of letter 'e'

---
+++ StringMethodenForms/opdracht1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace StringMethodenForms
+{
+    partial class opdracht1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OkButton = new System.Windows.Forms.Button();
+            this.Label = new System.Windows.Forms.Label();
+            this.MessageBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(398, 111);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(104, 39);
+            this.OkButton.TabIndex = 0;
+            this.OkButton.Text = "Ok";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(90, 73);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(60, 13);
+            this.Label.TabIndex = 1;
+            this.Label.Text = "Type hier...";
+            // 
+            // MessageBox
+            // 
+            this.MessageBox.Location = new System.Drawing.Point(160, 70);
+            this.MessageBox.Name = "MessageBox";
+            this.MessageBox.Size = new System.Drawing.Size(187, 20);
+            this.MessageBox.TabIndex = 2;
+            // 
+            // opdracht1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 171);
+            this.Controls.Add(this.MessageBox);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.OkButton);
+            this.Name = "opdracht1";
+            this.Text = "Index van ‘e’";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.TextBox MessageBox;
+    }
+}
---
+++ StringMethodenForms/opdracht1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringMethodenForms
+{
+    public partial class opdracht1 : Form
+    {
+        public opdracht1()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            string message = MessageBox.Text;
+            int index = message.IndexOf("e");
+            string indexText = Convert.ToString(index);
+
+            if (index == -1)
+            {
+                Label.Text = "-1";
+            }
+            else 
+            { 
+                Label.Text = indexText;
+            }
+
+
+        }
+    }
+}
