2f3023e19ccefb0ffae08b7a4e72325445eea4ad
yoomster <nperenboom@hotmail.com>
2024-04-03 16:48:57 +0200

    Added Winform proj for practise

---
+++ Haxor/HaxorForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Haxor
+{
+    partial class HaxorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OkButton = new System.Windows.Forms.Button();
+            this.Label = new System.Windows.Forms.Label();
+            this.MessageBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(373, 144);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(103, 37);
+            this.OkButton.TabIndex = 0;
+            this.OkButton.Text = "Ok";
+            this.OkButton.UseVisualStyleBackColor = true;
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(93, 85);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(35, 13);
+            this.Label.TabIndex = 1;
+            this.Label.Text = "label1";
+            // 
+            // MessageBox
+            // 
+            this.MessageBox.Location = new System.Drawing.Point(186, 85);
+            this.MessageBox.Name = "MessageBox";
+            this.MessageBox.Size = new System.Drawing.Size(165, 20);
+            this.MessageBox.TabIndex = 2;
+            // 
+            // HaxorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 217);
+            this.Controls.Add(this.MessageBox);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.OkButton);
+            this.Name = "HaxorForm";
+            this.Text = "HaxorForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.TextBox MessageBox;
+    }
+}
