0fae7942161f1bb12fea8206999b6d6e61e73ac0
yoomster <nperenboom@hotmail.com>
2024-04-03 21:17:22 +0200

    Added WinForm project that gives personalised hi message

---
+++ HalloJij/HelloForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace HalloJij
+{
+    partial class HelloForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.ManButton = new System.Windows.Forms.RadioButton();
+            this.WomanButton = new System.Windows.Forms.RadioButton();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(91, 56);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(106, 13);
+            this.NameLabel.TabIndex = 9;
+            this.NameLabel.Text = "Voor- en achternaam";
+            // 
+            // ManButton
+            // 
+            this.ManButton.AutoSize = true;
+            this.ManButton.Location = new System.Drawing.Point(203, 128);
+            this.ManButton.Name = "ManButton";
+            this.ManButton.Size = new System.Drawing.Size(46, 17);
+            this.ManButton.TabIndex = 8;
+            this.ManButton.TabStop = true;
+            this.ManButton.Text = "Man";
+            this.ManButton.UseVisualStyleBackColor = true;
+            // 
+            // WomanButton
+            // 
+            this.WomanButton.AutoSize = true;
+            this.WomanButton.Location = new System.Drawing.Point(203, 94);
+            this.WomanButton.Name = "WomanButton";
+            this.WomanButton.Size = new System.Drawing.Size(55, 17);
+            this.WomanButton.TabIndex = 7;
+            this.WomanButton.TabStop = true;
+            this.WomanButton.Text = "Vrouw";
+            this.WomanButton.UseVisualStyleBackColor = true;
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(588, 152);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(98, 35);
+            this.OkButton.TabIndex = 6;
+            this.OkButton.Text = "Ok";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(203, 49);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(242, 20);
+            this.NameBox.TabIndex = 5;
+            // 
+            // HelloForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 236);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.ManButton);
+            this.Controls.Add(this.WomanButton);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.NameBox);
+            this.Name = "HelloForm";
+            this.Text = "HelloForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.RadioButton ManButton;
+        private System.Windows.Forms.RadioButton WomanButton;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.TextBox NameBox;
+    }
+}
---
+++ HalloJij/HelloForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HalloJij
+{
+    public partial class HelloForm : Form
+    {
+        public HelloForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            //voor- en achternaam En vinkt een radiobutton geslacht
+            //
+            //programma toont MessageBox met de tekst “Hallo mejuffrouw Jansen” of “Hallo mijnheer Jansen”
+            //afhankelijk van de ingevoerde tekst en geselecteerde radiobutton. 
+            //De gebruiker voert Voornaam +Achternaam in, maar de MessageBox toont de tekst zonder de voornaam.
+            //Invoer als “Jan de Graaf” en radiobutton geslacht “Man“ levert het bericht “Hallo mijnheer de Graaf” op.
+
+
+        }
+    }
+}
