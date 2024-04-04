319c0ab3152134e054bd7ca09ec49c7fb66e060e
yoomster <nperenboom@hotmail.com>
2024-04-04 18:19:52 +0200

    Added a WinForm for throwing dice

---
+++ WorpenGenerator/WorpenGeneratorForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WorpenGenerator
+{
+    partial class WorpenGeneratorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResultBox = new System.Windows.Forms.GroupBox();
+            this.ThrowsLabel = new System.Windows.Forms.Label();
+            this.diceThrowsList = new System.Windows.Forms.ListBox();
+            this.SettingsBox = new System.Windows.Forms.GroupBox();
+            this.AmountOfThrowsLabel = new System.Windows.Forms.Label();
+            this.PipsLabel = new System.Windows.Forms.Label();
+            this.ThrowsNumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.PipsNumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.ThrowDiceButton = new System.Windows.Forms.Button();
+            this.TotalPipsLabel = new System.Windows.Forms.Label();
+            this.ResultBox.SuspendLayout();
+            this.SettingsBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ThrowsNumUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipsNumUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ResultBox
+            // 
+            this.ResultBox.Controls.Add(this.TotalPipsLabel);
+            this.ResultBox.Controls.Add(this.ThrowsLabel);
+            this.ResultBox.Controls.Add(this.diceThrowsList);
+            this.ResultBox.Location = new System.Drawing.Point(352, 45);
+            this.ResultBox.Name = "ResultBox";
+            this.ResultBox.Size = new System.Drawing.Size(235, 268);
+            this.ResultBox.TabIndex = 0;
+            this.ResultBox.TabStop = false;
+            this.ResultBox.Text = "Resultaat";
+            // 
+            // ThrowsLabel
+            // 
+            this.ThrowsLabel.AutoSize = true;
+            this.ThrowsLabel.Location = new System.Drawing.Point(6, 25);
+            this.ThrowsLabel.Name = "ThrowsLabel";
+            this.ThrowsLabel.Size = new System.Drawing.Size(48, 13);
+            this.ThrowsLabel.TabIndex = 1;
+            this.ThrowsLabel.Text = "Worpen:";
+            // 
+            // diceThrowsList
+            // 
+            this.diceThrowsList.FormattingEnabled = true;
+            this.diceThrowsList.Location = new System.Drawing.Point(6, 54);
+            this.diceThrowsList.Name = "diceThrowsList";
+            this.diceThrowsList.Size = new System.Drawing.Size(219, 173);
+            this.diceThrowsList.TabIndex = 0;
+            // 
+            // SettingsBox
+            // 
+            this.SettingsBox.Controls.Add(this.ThrowDiceButton);
+            this.SettingsBox.Controls.Add(this.PipsNumUpDown);
+            this.SettingsBox.Controls.Add(this.ThrowsNumUpDown);
+            this.SettingsBox.Controls.Add(this.PipsLabel);
+            this.SettingsBox.Controls.Add(this.AmountOfThrowsLabel);
+            this.SettingsBox.Location = new System.Drawing.Point(27, 83);
+            this.SettingsBox.Name = "SettingsBox";
+            this.SettingsBox.Size = new System.Drawing.Size(293, 113);
+            this.SettingsBox.TabIndex = 1;
+            this.SettingsBox.TabStop = false;
+            this.SettingsBox.Text = "Instellingen";
+            // 
+            // AmountOfThrowsLabel
+            // 
+            this.AmountOfThrowsLabel.AutoSize = true;
+            this.AmountOfThrowsLabel.Location = new System.Drawing.Point(3, 16);
+            this.AmountOfThrowsLabel.Name = "AmountOfThrowsLabel";
+            this.AmountOfThrowsLabel.Size = new System.Drawing.Size(78, 13);
+            this.AmountOfThrowsLabel.TabIndex = 0;
+            this.AmountOfThrowsLabel.Text = "Aantal worpen:";
+            // 
+            // PipsLabel
+            // 
+            this.PipsLabel.AutoSize = true;
+            this.PipsLabel.Location = new System.Drawing.Point(6, 48);
+            this.PipsLabel.Name = "PipsLabel";
+            this.PipsLabel.Size = new System.Drawing.Size(67, 13);
+            this.PipsLabel.TabIndex = 2;
+            this.PipsLabel.Text = "Aantal ogen:";
+            // 
+            // ThrowsNumUpDown
+            // 
+            this.ThrowsNumUpDown.Location = new System.Drawing.Point(153, 16);
+            this.ThrowsNumUpDown.Name = "ThrowsNumUpDown";
+            this.ThrowsNumUpDown.Size = new System.Drawing.Size(83, 20);
+            this.ThrowsNumUpDown.TabIndex = 3;
+            // 
+            // PipsNumUpDown
+            // 
+            this.PipsNumUpDown.Location = new System.Drawing.Point(154, 44);
+            this.PipsNumUpDown.Name = "PipsNumUpDown";
+            this.PipsNumUpDown.Size = new System.Drawing.Size(81, 20);
+            this.PipsNumUpDown.TabIndex = 4;
+            // 
+            // ThrowDiceButton
+            // 
+            this.ThrowDiceButton.Location = new System.Drawing.Point(102, 70);
+            this.ThrowDiceButton.Name = "ThrowDiceButton";
+            this.ThrowDiceButton.Size = new System.Drawing.Size(120, 23);
+            this.ThrowDiceButton.TabIndex = 5;
+            this.ThrowDiceButton.Text = "Gooi dobbelsteen";
+            this.ThrowDiceButton.UseVisualStyleBackColor = true;
+            // 
+            // TotalPipsLabel
+            // 
+            this.TotalPipsLabel.AutoSize = true;
+            this.TotalPipsLabel.Location = new System.Drawing.Point(21, 239);
+            this.TotalPipsLabel.Name = "TotalPipsLabel";
+            this.TotalPipsLabel.Size = new System.Drawing.Size(99, 13);
+            this.TotalPipsLabel.TabIndex = 2;
+            this.TotalPipsLabel.Text = "Totaal aantal ogen:";
+            // 
+            // WorpenGeneratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 339);
+            this.Controls.Add(this.SettingsBox);
+            this.Controls.Add(this.ResultBox);
+            this.Name = "WorpenGeneratorForm";
+            this.Text = "Dobbelsteen worpengenerator";
+            this.ResultBox.ResumeLayout(false);
+            this.ResultBox.PerformLayout();
+            this.SettingsBox.ResumeLayout(false);
+            this.SettingsBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ThrowsNumUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipsNumUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox ResultBox;
+        private System.Windows.Forms.Label ThrowsLabel;
+        private System.Windows.Forms.ListBox diceThrowsList;
+        private System.Windows.Forms.GroupBox SettingsBox;
+        private System.Windows.Forms.Button ThrowDiceButton;
+        private System.Windows.Forms.NumericUpDown PipsNumUpDown;
+        private System.Windows.Forms.NumericUpDown ThrowsNumUpDown;
+        private System.Windows.Forms.Label PipsLabel;
+        private System.Windows.Forms.Label AmountOfThrowsLabel;
+        private System.Windows.Forms.Label TotalPipsLabel;
+    }
+}
---
+++ WorpenGenerator/WorpenGeneratorForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorpenGenerator
+{
+    public partial class WorpenGeneratorForm : Form
+    {
+        public WorpenGeneratorForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
